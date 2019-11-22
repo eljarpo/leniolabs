ebfed606a1271d13d919270111e61d7de8e90092
Jorge Romussi <jromussi@gmail.com>
2019-11-22 18:30:28 -0300

    Updates

---
+++ precache-manifest.91718733d109ae2324200059d561aa98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed58314e16da184d507e5348ca03e92e",
+    "url": "/leniolabs/index.html"
+  },
+  {
+    "revision": "81dc84b438d17974769c",
+    "url": "/leniolabs/static/css/2.15314edf.chunk.css"
+  },
+  {
+    "revision": "6400990b0fbab3bdce85",
+    "url": "/leniolabs/static/css/main.8d3da61b.chunk.css"
+  },
+  {
+    "revision": "81dc84b438d17974769c",
+    "url": "/leniolabs/static/js/2.53f4066e.chunk.js"
+  },
+  {
+    "revision": "6400990b0fbab3bdce85",
+    "url": "/leniolabs/static/js/main.fe0c0522.chunk.js"
+  },
+  {
+    "revision": "22dd5fb4c3690193842f",
+    "url": "/leniolabs/static/js/runtime-main.22362ee6.js"
+  }
+]);
